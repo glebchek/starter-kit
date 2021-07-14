892950c4c59f74a4833050a2dd25d97a7cb7822c
Gleb Chekushin <mail@glebchek.com>
2021-07-14 13:58:57 +0500

    Remove example component and react-router-dom as it is not always needed

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,16 +1,7 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-
-import Greeting from '../greeting';
 
 export default function App(): JSX.Element {
   return (
-    <Router>
-      <Switch>
-        <Route exact path={['/', '/user/:name']}>
-          <Greeting />
-        </Route>
-      </Switch>
-    </Router>
+    <div/>
   );
 }
